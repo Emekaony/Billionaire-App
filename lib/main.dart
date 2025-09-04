da3c70e27df24c42a2020c9390e44ddf54bdacf3
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2025-09-03 20:33:49 -0700

    So we learned about the expanded widget coupled with the flex property

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark(),
+      theme: ThemeData.dark(useMaterial3: true),
       home: Scaffold(
         appBar: AppBar(title: Text("Billionaire App")),
         body: Container(
@@ -26,12 +26,30 @@ class MyApp extends StatelessWidget {
           height: double.infinity,
           color: Colors.blueGrey[700],
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text("Text Part"),
-              ElevatedButton(
-                onPressed: onButtonPressed,
-                child: Text("Click Me!"),
+              Expanded(
+                flex: 9,
+                child: SizedBox(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Bank balance:"),
+                      SizedBox(height: 10),
+                      Text("\$200,000"),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.red[700],
+                    minimumSize: Size(double.infinity, 0),
+                  ),
+                  onPressed: onButtonPressed,
+                  child: Text("Click me!"),
+                ),
               ),
             ],
           ),
